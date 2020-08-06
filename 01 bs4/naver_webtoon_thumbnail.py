37097301b6be8de7cff705c57c0d2b2c80b6c9d5
SeungHoon Jang <jsh1400512@naver.com>
2020-08-07 02:14:13 +0900

    update thumbnail.py

--- 01 bs4/naver_webtoon_thumbnail.py
+++ 01 bs4/naver_webtoon_thumbnail.py
@@ -3,7 +3,13 @@ from pprint import pprint
 import requests
 
 html = requests.get("https://comic.naver.com/webtoon/weekday.nhn")
-soup = BeautifulSoup(html,'html.parser')
+soup = BeautifulSoup(html.text,'html.parser')
 html.close()
 
-data1 = soup.findAll('a',{'class':'title'})
+data1_list = soup.findAll('div',{'class':'col_inner'})
+
+li_list = []
+for data1 in data1_list:
+    li_list.extend(data1.findAll('li'))
+
+pprint(li_list)
