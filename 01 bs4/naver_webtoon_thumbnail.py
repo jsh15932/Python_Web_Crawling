441b26e1cbd2145e6dbf1583ac797d52d43cd513
장승훈 <jsh1400512@naver.com>
2020-08-07 04:25:23 +0900

    Commit

--- 01 bs4/naver_webtoon_thumbnail.py
+++ 01 bs4/naver_webtoon_thumbnail.py
@@ -1,15 +1,21 @@
 from bs4 import BeautifulSoup
 from pprint import pprint
 import requests
+from urllib.request import urlretrieve
 
 html = requests.get("https://comic.naver.com/webtoon/weekday.nhn")
-soup = BeautifulSoup(html.text,'html.parser')
+soup = BeautifulSoup(html.text, 'html.parser')
 html.close()
 
-data1_list = soup.findAll('div',{'class':'col_inner'})
+data1_list = soup.findAll('div', {'class':'col_inner'})
 
 li_list = []
 for data1 in data1_list:
     li_list.extend(data1.findAll('li'))
 
-pprint(li_list)
+for li in li_list:
+    img = li.find('img')
+    title = img['title']
+    img_src = img['src']
+    
+    urlretrieve(img_src, title+'.jpg')
